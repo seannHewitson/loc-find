942f483bf091f31800643347961eee71c38fe64a
Sean Hewitson <shewitson@webgains.com>
2022-04-08 15:49:48 +0100

    LOC-88: feat - Updated script to search for keys removing the last part.

--- index.ts
+++ index.ts
@@ -1,51 +1,45 @@
 import fs from 'fs'
 import path from 'path'
-import { scanFolder } from './scanner'
 
-const found = {
-    api: 0,
-    ui: 0,
-    ad: 0,
-    legacy: 0,
+import { scanFolder } from './scanner'
+import keys from './keys'
+
+const repos: any = {
+    platformUI: 'Platform-UI-Prototype',
+    platformAPI: 'Platform-API',
+    legacy: 'old-platform'
 }
 
-const keys = fs.readFileSync(
-    path.resolve(__dirname, 'loc.csv')
-).toString().split('\r\n').map((key: string) => ({
-    key,
-    api: false,
-    ui: false,
-    ad: false,
-    legacy: false,
-}))
-
-const root = path.resolve(__dirname, '../',)
-const exclude = ['node_modules', '.git', 'web', 'lokalise-tmp', 'images', 'mosaico', 'vendor', 'storage']
+const found: any = {}
+Object.keys(repos).forEach((repo: string) => (found[repo] = 0))
 
 const results = keys.map(({ key }: any) => {
-    const api = scanFolder(path.resolve(root, 'Platform-API'), key, exclude)
-    const ui = scanFolder(path.resolve(root, 'Platform-UI-Prototype'), key, exclude)
-    const ad = scanFolder(path.resolve(root, 'ui'), key, exclude)
-    const legacy = scanFolder(path.resolve(root, 'old-platform'), key, exclude)
-    found.api += Number(api)
-    found.ui += Number(ui)
-    found.ad += Number(ad)
-    found.legacy += Number(legacy)
-    return { key, api, ui, ad, legacy }
+    const result: any = { key }
+    Object.keys(repos).forEach((repo: string) => {
+        result[repo] = scanFolder(
+            path.resolve(__dirname, '../', repos[repo]),
+            key
+        )
+        found[repo] += Number(result[repo])
+    })
+    return result
 })
 
-let output: string = ''
-results.forEach(({ key, api, ui, ad, legacy }: any) => {
-    output += `${key},${api},${ui},${ad},${legacy}\r\n`
-})
+if (results.length) {
+    let output: string = `${Object.keys(results[0]).join(',')}\r\n`
+    results.forEach((result: any) => (
+        output += `${
+            Object.keys(result)
+                .map((key: string) => result[key])
+                .join(',')
+        }\r\n`
+    ))
+    
+    fs.writeFileSync(
+        path.resolve(__dirname, 'output.csv'),
+        output,
+        { encoding: 'utf-8', flag: 'w' }
+    )
+}
 
-fs.writeFileSync(
-    path.resolve(__dirname, 'output.csv'),
-    output,
-    { encoding: 'utf-8', flag: 'w' }
-)
-
-console.log(`Found api: ${found.api}`)
-console.log(`Found ui: ${found.ui}`)
-console.log(`Found ad: ${found.ad}`)
-console.log(`Found legacy: ${found.legacy}`)
+console.log({ found })
--- scanner.ts
+++ scanner.ts
@@ -8,17 +8,26 @@ export const scanFile = (file: string, key: string): boolean => {
     return expr.test(contents) || expr1.test(contents)
 }
 
-export const scanFolder = (folder: string, key: string, exclude?: Array<string>): boolean => {
-    let found = false
+const exclude: Array<string> = [
+    'node_modules',
+    '.git',
+    'web',
+    'lokalise-tmp',
+    'images',
+    'mosaico',
+    'vendor',
+    'storage'
+]
 
+export const scanFolder = (folder: string, key: string): boolean => {
+    let found = false
     const readDir = fs.readdirSync(folder)
     for (let filename of readDir) {
         if (!exclude || !exclude.includes(filename)) {
             const file = path.resolve(folder, filename)
-            console.log(file)
             const stats = fs.statSync(file)
             if (stats.isFile()) found = scanFile(file, key)
-            if (stats.isDirectory()) found = scanFolder(file, key, exclude)
+            if (stats.isDirectory()) found = scanFolder(file, key)
             if (found) {
                 break
             }
